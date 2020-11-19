37b7da950845d81fce7e2b4a3356402ae0a22e6d
uHan <asgd56@gmail.com>
2020-11-19 15:04:02 +0900

    sign up

--- src/main/java/com/covidPotar19/controller/MemberController.java
+++ src/main/java/com/covidPotar19/controller/MemberController.java
@@ -1,10 +1,16 @@
 package com.covidPotar19.controller;
 
+import com.covidPotar19.domain.Member;
+import com.covidPotar19.domain.MemberForm;
 import com.covidPotar19.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class MemberController
@@ -16,18 +22,48 @@ public class MemberController
         this.memberService = memberService;
     }
 
-    @GetMapping(value = "/signInForm")
-    public String signInForm() {
+    @GetMapping("/signInForm")
+    public String signInForm()
+    {
         return "signInForm";
     }
 
-    @GetMapping(value = "/signUpForm")
-    public String SignInController() {
-        return "signInForm";
+    @GetMapping("signUpForm")
+    public String SignUpForm()
+    {
+        return "signUpForm";
     }
 
     @RequestMapping(value = "/index")
-    public String indexController() {
+    public String indexController()
+    {
         return "index";
     }
+
+    @GetMapping(value = "/members/new")
+    public String createForm(Model model)
+    {
+        model.addAttribute("memberForm", new MemberForm());
+        return "signUpForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String joinMember(@Valid MemberForm memberForm, BindingResult bindingResult)
+    {
+        if(bindingResult.hasErrors())
+        {
+            return "signUpForm";
+        }
+
+        Member member = new Member();
+
+        member.setName(memberForm.getName());
+        member.setUserId(memberForm.getUserId());
+        member.setPassword(memberForm.getPassword());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
 }
---
+++ src/main/java/com/covidPotar19/domain/MemberForm.java
@@ -0,0 +1,45 @@
+package com.covidPotar19.domain;
+
+import javax.validation.constraints.NotEmpty;
+
+public class MemberForm
+{
+    @NotEmpty(message = "회원 이름은 필수 입니다")
+    private String name;
+
+    @NotEmpty(message = "회원 아이디는 필수 입니다")
+    private String userId;
+
+    @NotEmpty(message = "회원 비밀번호는 필수 입니다")
+    private String password;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getUserId()
+    {
+        return userId;
+    }
+
+    public void setUserId(String userId)
+    {
+        this.userId = userId;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
+    }
+}
